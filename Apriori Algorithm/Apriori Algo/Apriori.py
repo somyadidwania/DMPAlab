9e4791f2a18315989b2c6955393fe08bec21de1f
Somya Didwania <somyadidwania@gmail.com>
2019-04-10 23:18:09 +0530

    Pruning corrected

--- Apriori Algorithm/Apriori Algo/Apriori.py
+++ Apriori Algorithm/Apriori Algo/Apriori.py
@@ -1,17 +1,13 @@
 import csv
 from itertools import combinations
-
+L_final = []
 def support_count(data,C_old):
 	cnt = 0
-	#print('  C_old  ',C_old)
-	#print('  data  ',data)
 	C_new = {}
 	for i in data:
 		for itemset in C_old:
-			#print(itemset,' is in ',C_old,' i is : ',i)
 			if set(itemset).issubset(i):
 				C_new[itemset] = C_new.get(itemset,0)+1
-				#print('C_new is ',C_new)
 	C_new = dict(sorted(C_new.items()))
 	return C_new
 
@@ -30,9 +26,7 @@ def generate_C(items,i):
 	else:
 		C_gen = [] 
 		for x in range(0,len(items)):
-			#print('X is  ',items[x])
 			for y in range(x+1,len(items)):
-				#print('Y is  ',items[y])
 				if(items[x][0:(i-2)] == items[y][0:(i-2)]):
 					t = list(items[x][0:(i-2)])
 					t.append(items[x][(i-2)])
@@ -44,24 +38,22 @@ def prune(C,i,tran_1,min_sup):
 		subsets_of_C = []
 		pruned_C = []
 		for key in C:
-			for j in range(1,i): 
-					subsets_of_C = subsets_of_C + list(combinations(key,j))
-			subsets_of_C = support_count(tran_1,subsets_of_C)
-			if(all(x >= min_sup for x in subsets_of_C.values())):
+			subsets_of_C = subsets_of_C + list(combinations(key,i-1))
+			print("Subset od c = ",subsets_of_C)
+			if  [x for x in subsets_of_C if x in L_final] == subsets_of_C:
 				pruned_C.append(key)
 			print('Subset of C is :', subsets_of_C)
 			subsets_of_C = []
 
-		print('pruned C is :',pruned_C)
+		print('Pruned C',i,'is :',pruned_C)
 		return pruned_C
 
 #Reading of csv file
-csv.register_dialect('myDialect',delimiter = ',',skipinitialspace=True) #Setting the delimiter
 file_name = input("Enter file name: ")
 tran = list()
 C1 = {}
 with open(file_name) as csv_file:
-	data = csv.reader(csv_file, dialect = 'myDialect')
+	data = csv.reader(csv_file,delimiter = ",")
 	for row in data:
 		for i in range(1,len(row)):
 			if row[i] is not '':
@@ -70,7 +62,7 @@ with open(file_name) as csv_file:
 print(tran)
 C1 = dict(sorted(C1.items()))
 print("C1 item set is ", C1)
-L_final = []
+
 min_sup = int(input("Enter the minimum support : "))
 #L1 generation
 L1 = sup_cnt_comparison(C1,min_sup)
@@ -91,7 +83,7 @@ C = generate_C(list(L2.keys()),i)
 print('C',i,' is : ',C)
 while len(C) > 0:
 	pruned_C = prune(C,i,tran,min_sup)
-	print('Puned C',i,' is : ',pruned_C)
+	C=pruned_C
 	if len(pruned_C)>0:
 		C = support_count(tran,C)
 		print('C',i,' is : ',C)
